a2094eddb14fb32d2b7fd72b86a90b71f69625ee
Pakhomov <pahomov.1v@yandex.ru>
2020-11-20 16:09:57 +0700

    AvengersMovieDetails

--- app/src/main/java/ru/navifromnorth/homeworking/MovieDetailsActivity.kt
+++ app/src/main/java/ru/navifromnorth/homeworking/MovieDetailsActivity.kt
@@ -5,15 +5,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
 
-class MainActivity : AppCompatActivity() {
+class MovieDetailsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_movie_details)
 
-        val textView: TextView = findViewById(R.id.avengers)
+        val backButton: TextView = findViewById(R.id.BackButton)
 
-        textView.setOnClickListener{
-            val intent = Intent(this, MovieDetailsActivity::class.java)
+        backButton.setOnClickListener{
+            val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
     }
